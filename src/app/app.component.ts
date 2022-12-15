a46aae6b17b9e5f17356fdbcbd8d18d68019b34f
Orcrax <matthieu.rebate@gmail.com>
2022-12-15 14:08:54 +0100

    init Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'PokeProject';
+  title = 'Pokédex';
+  name = 'Orcrax';
+  text = "Coucou !";
+  
 }
+
+
